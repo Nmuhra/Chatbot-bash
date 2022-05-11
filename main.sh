0720ae5504063c63841191adf95ecb06db604135
Nabegh <97016646+Nmuhra@users.noreply.github.com>
2022-05-11 18:00:26 +0000

    Initial commit

---
+++ main.sh
@@ -0,0 +1,18 @@
+echo "Hello welcome to my website do you require any help?"
+
+sleep 3
+
+echo "Here is what I know:
+Press on the links for redirects
+press on the GitHub logo to get to Nabegh's Github"
+
+echo "I will call a human to assist you because I am just
+a chatbot after all:("
+
+sleep 5
+
+echo "they are on their way..."
+
+sleep 2 
+
+echo "Thanks for your patience"
